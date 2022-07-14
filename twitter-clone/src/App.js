668ca58e4c54df7fe482c20f08c54918018bb0a8
DouglasPenn23 <Douglaspennington2013@gmail.com>
2022-07-14 13:13:12 -0400

    Added Icons for UI

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,4 +1,6 @@
 
+import React from 'react';
+import Sidebar from './Sidebar';
 import './App.css';
 
 function App() {
@@ -6,6 +8,14 @@ function App() {
     // Following BEM convention
     <div className="app">
      <h1> Hey people, let's build a twitter clone! </h1>
+
+    {/* Sidebar */}
+    <Sidebar />
+
+    {/* Feed */}
+
+
+    {/* Widgets */}
     </div>
   );
 }
