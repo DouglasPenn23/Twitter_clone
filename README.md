0e365a7273d20c4862b1ee1f6bf5ce46c7d55f97
DouglasPenn23 <Douglaspennington2013@gmail.com>
2022-07-14 09:28:19 -0400

    Cleanup process done, getting started

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Twitter_clone
+- MaterialUI used for all icons & designs
+- Firebase used from Google to manage hosting
+
+
+
+Commits by points in YT:
+Cleanup Process Push - 24:48
---
+++ twitter-clone/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    // Following BEM convention
+    <div className="app">
+     <h1> Hey people, let's build a twitter clone! </h1>
+    </div>
+  );
+}
+
+export default App;
